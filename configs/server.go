8f1cedb76086ac0f2f51a4e8aa1074e156e5527a
Respati Tri Susetyo <tyorespatiaja@gmail.com>
2024-07-02 14:20:16 +0700

    ci

--- configs/server.go
+++ configs/server.go
@@ -1,6 +1,9 @@
 package configs
 
 import (
+	"http"
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 	docs "github.com/respati123/money-tracking/docs"
 	"github.com/respati123/money-tracking/internal/handlers"
@@ -34,13 +37,16 @@ func RunServer(env util.Config, db gorm.DB) {
 	router.GET("swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
 	router.Run(":" + env.PORT_SERVER)
-	// create route here;
 
-	// versionOne := router.Group("/v1/")
-	// {
-	// 	authRouter := versionOne.Group("auth")
-	// 	{
-	// 			authRouter.POST("/login", fun)
-	// 	}
-	// }
+	versionOne := router.Group("/v1/")
+	{
+		authRouter := versionOne.Group("auth")
+		{
+			authRouter.POST("/login", func(ctx *gin.Context) {
+				ctx.JSON(http.StatusOK, gin.H{
+					"success": true,
+				})
+			})
+		}
+	}
 }
